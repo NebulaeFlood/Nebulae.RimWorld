6c36d23c72027b43d104a23d73fc6babbb54c557
Eternal Twilight <2975150170@qq.com>
2024-12-21 18:14:49 +0800

    修改打包脚本。

--- Pack.cmd
+++ Pack.cmd
@@ -3,4 +3,5 @@
 dotnet build --configuration Release
 dotnet build --configuration Debug
 
-nuget pack .\Nebulae.RimWorld.nuspec -OutputDirectory .\buildedPackages
+nuget pack .\Nebulae.RimWorld.nuspec -OutputDirectory .\build
+nuget pack .\Nebulae.RimWorld.UI.nuspec -OutputDirectory .\build
