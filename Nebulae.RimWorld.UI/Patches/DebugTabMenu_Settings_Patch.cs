59b455e8ac7a2ed1458600f969102ebaebaf15ca
Eternal Twilight <2975150170@qq.com>
2025-01-06 15:13:25 +0800

    向原版窗口添加控件调试辅助选项。

---
+++ Nebulae.RimWorld.UI/Patches/DebugTabMenu_Settings_Patch.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using LudeonTK;
+using Nebulae.RimWorld.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nebulae.RimWorld.UI.Patches
+{
+    [HarmonyPatch(typeof(DebugTabMenu_Settings), nameof(DebugTabMenu_Settings.InitActions))]
+    internal class DebugTabMenu_Settings_Patch
+    {
+        [HarmonyPostfix]
+        internal static void InitActionsPostfix(DebugTabMenu_Settings __instance)
+        {
+            Type type = typeof(Control);
+            MethodInfo method = AccessTools.Method(typeof(DebugTabMenu_Settings), "AddNode");
+
+            AccessTools.Method(typeof(DebugTabMenu_Settings), "AddNode").Invoke(__instance, new object[]
+            {
+                AccessTools.Field(type, nameof(Control.ClickForceArrange)),
+                "Aether Control"
+            });
+            AccessTools.Method(typeof(DebugTabMenu_Settings), "AddNode").Invoke(__instance, new object[]
+            {
+                AccessTools.Field(type, nameof(Control.ClickForceMeasure)),
+                "Aether Control"
+            });
+            AccessTools.Method(typeof(DebugTabMenu_Settings), "AddNode").Invoke(__instance, new object[]
+            {
+                AccessTools.Field(type, nameof(Control.DrawFullRegion)),
+                "Aether Control"
+            });
+            AccessTools.Method(typeof(DebugTabMenu_Settings), "AddNode").Invoke(__instance, new object[]
+            {
+                AccessTools.Field(type, nameof(Control.DrawRegion)),
+                "Aether Control"
+            });
+            AccessTools.Method(typeof(DebugTabMenu_Settings), "AddNode").Invoke(__instance, new object[]
+            {
+                AccessTools.Field(type, nameof(Control.ShowInfo)),
+                "Aether Control"
+            });
+        }
+    }
+}
---
+++ Nebulae.RimWorld.UI/UIPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Nebulae.RimWorld.UI
+{
+    [StaticConstructorOnStartup]
+    internal static class UIPatch
+    {
+        internal const string UniqueId = "Nebulae.RimWorld.UI";
+
+        internal static readonly Harmony HarmonyInstance;
+
+        static UIPatch()
+        {
+            HarmonyInstance = new Harmony(UniqueId);
+            HarmonyInstance.PatchAll();
+        }
+    }
+}
