10c4de3db177825820c71104ee6e4226fdb62b6b
NebulaeFlood <2975150170@qq.com>
2025-01-15 10:27:04 +0800

    拆分自动打包批处理文件。

--- Pack-UI.cmd
+++ Pack-UI.cmd
@@ -1,7 +1,6 @@
-
 dotnet build --configuration Release
 % dotnet build --configuration Debug %
 
-nuget pack .\Nebulae.RimWorld.nuspec -OutputDirectory .\build
+nuget pack .\Nebulae.RimWorld.UI.nuspec -OutputDirectory .\build
 
 start explorer.exe .\build
