5161a5e8486ba1b0d909812f3d703a127a84d2fc
NebulaeFlood <2975150170@qq.com>
2025-02-19 21:49:33 +0800

    修复无法生成控件的问题。

--- Nebulae.RimWorld.UI/Utilities/ModSettingLayoutUtility.cs
+++ Nebulae.RimWorld.UI/Utilities/ModSettingLayoutUtility.cs
@@ -1,6 +1,9 @@
-﻿using Nebulae.RimWorld.UI.Controls;
+﻿using HarmonyLib;
+using Nebulae.RimWorld.UI.Controls;
 using Nebulae.RimWorld.UI.Converters;
+using Nebulae.RimWorld.UI.Data;
 using Nebulae.RimWorld.UI.Data.Binding;
+using Nebulae.RimWorld.Utilities;
 using System;
 using System.Reflection;
 using UnityEngine;
@@ -26,33 +29,24 @@ namespace Nebulae.RimWorld.UI.Utilities
         /// <exception cref="MissingFieldException">当无法在 <typeparamref name="T"/> 中找到名为 <paramref name="entryName"/> 的字段时发生。</exception>
         public static CheckBox CreateBooleanEntry<T>(T settings, string entryName, string label, string tooltip) where T : ModSettings
         {
-            if (typeof(T).GetField(entryName, BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-                is FieldInfo field)
+            bool value = typeof(T).GetValue<bool>(entryName, settings);
+            var checkBox = new CheckBox
             {
-                bool value = field.GetValue(settings) is true;
+                Status = value ? ToggleStatus.Checked : ToggleStatus.Unchecked,
+                Text = label,
+                Tooltip = tooltip,
+                ShowTooltip = !string.IsNullOrEmpty(tooltip)
+            };
 
-                var checkBox = new CheckBox
-                {
-                    Status = value ? ToggleStatus.Checked : ToggleStatus.Unchecked,
-                    Text = label,
-                    Tooltip = tooltip,
-                    ShowTooltip = string.IsNullOrEmpty(tooltip)
-                };
+            BindingManager.Bind(
+                checkBox,
+                ToggleButton.StatusProperty,
+                settings,
+                entryName,
+                ToggleStatusConverter.Instance,
+                BindingMode.OneWay);
 
-                BindingManager.Bind(
-                    checkBox,
-                    ToggleButton.StatusProperty,
-                    settings,
-                    entryName,
-                    ToggleStatusConverter.Instance,
-                    BindingMode.OneWay);
-
-                return checkBox;
-            }
-            else
-            {
-                throw new MissingFieldException(typeof(T).Name, entryName);
-            }
+            return checkBox;
         }
 
         /// <summary>
@@ -71,66 +65,56 @@ namespace Nebulae.RimWorld.UI.Utilities
         /// <exception cref="MissingFieldException">当无法在 <typeparamref name="T"/> 中找到名为 <paramref name="entryName"/> 的字段时发生。</exception>
         public static Grid CreateNumberEntry<T>(T settings, string entryName, string label, string tooltip, float minValue, float maxValue, int decimalPartDigit, float silderStep) where T : ModSettings
         {
-            if (typeof(T).GetField(entryName, BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-                is FieldInfo field)
+            float value = typeof(T).GetValue<float>(entryName, settings);
+
+            var entryLabel = new Label
             {
-                bool showTooltip = string.IsNullOrEmpty(tooltip);
-                float value = (float)field.GetValue(settings);
+                Anchor = TextAnchor.MiddleLeft,
+                Margin = new Thickness(0f, 8f, 0f, 0f),
+                Text = label
+            };
 
-                var entryLabel = new Label
-                {
-                    Anchor = TextAnchor.MiddleLeft,
-                    Margin = new Thickness(0f, 8f, 0f, 0f),
-                    Text = label,
-                    Tooltip = tooltip,
-                    ShowTooltip = showTooltip
-                };
-
-                var numberBox = new NumberBox
-                {
-                    Margin = new Thickness(0f, 8f, 0f, 0f),
-                    DecimalPartDigit = decimalPartDigit,
-                    Maximum = maxValue,
-                    Minimum = minValue,
-                    Value = value,
-                    Tooltip = tooltip,
-                    ShowTooltip = showTooltip
-                };
-
-                var slider = new Slider
-                {
-                    Maximum = maxValue,
-                    Minimum = minValue,
-                    Step = silderStep,
-                    Value = value,
-                    Tooltip = tooltip,
-                    ShowTooltip = showTooltip
-                };
-
-                BindingManager.Bind(
-                    slider,
-                    Slider.ValueProperty,
-                    numberBox,
-                    NumberBox.ValueProperty,
-                    BindingMode.TwoWay);
-
-                BindingManager.Bind(
-                    slider,
-                    Slider.ValueProperty,
-                    settings,
-                    entryName,
-                    BindingMode.OneWay);
-
-                return new Grid().SetSize(new float[] { Grid.Remain, 100f }, new float[] { 32f, 16f })
-                    .Set(
-                        entryLabel, numberBox,
-                        slider, slider
-                    );
-            }
-            else
+            var numberBox = new NumberBox
             {
-                throw new MissingFieldException(typeof(T).Name, entryName);
+                Margin = new Thickness(0f, 8f, 0f, 0f),
+                DecimalPartDigit = decimalPartDigit,
+                Maximum = maxValue,
+                Minimum = minValue,
+                Value = value
+            };
+
+            var slider = new Slider
+            {
+                Maximum = maxValue,
+                Minimum = minValue,
+                Step = silderStep,
+                Value = value
+            };
+
+            BindingManager.Bind(
+                slider,
+                Slider.ValueProperty,
+                numberBox,
+                NumberBox.ValueProperty,
+                BindingMode.TwoWay);
+
+            BindingManager.Bind(
+                slider,
+                Slider.ValueProperty,
+                settings,
+                entryName,
+                BindingMode.OneWay);
+
+            return new Grid
+            {
+                Tooltip = tooltip,
+                ShowTooltip = string.IsNullOrEmpty(tooltip)
             }
+                .SetSize(new float[] { Grid.Remain, 100f }, new float[] { 32f, GameFont.Tiny.GetHeight() })
+                .Set(
+                    entryLabel, numberBox,
+                    slider, slider
+                );
         }
     }
 }
