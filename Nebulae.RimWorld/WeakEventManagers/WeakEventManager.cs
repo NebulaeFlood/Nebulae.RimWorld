628074268e73735b46473637b1b8a88e10d4bd3f
NebulaeFlood <2975150170@qq.com>
2025-02-21 10:32:27 +0800

    移动示例代码。

--- Nebulae.RimWorld/WeakEventManagers/WeakEventManager.cs
+++ Nebulae.RimWorld/WeakEventManagers/WeakEventManager.cs
@@ -181,7 +181,23 @@ namespace Nebulae.RimWorld.WeakEventManagers
         #endregion
     }
 
+
 #if DEBUG
+
+    /// <summary>
+    /// 表示一个订阅了由 <see cref="WeakEventManager{TSender, TArgs}"/> 管理的事件的对象
+    /// </summary>
+    /// <typeparam name="TSender">事件源类型</typeparam>
+    /// <typeparam name="TArgs">事件参数类型</typeparam>
+    public interface IWeakEventSubscriber<TSender, TArgs>
+    {
+        /// <summary>
+        /// 事件被触发时执行的操作
+        /// </summary>
+        /// <param name="sender">事件源</param>
+        /// <param name="args">事件数据</param>
+        void OnEventActivated(TSender sender, TArgs args);
+    }
 
     /// <summary>
     /// <see cref="WeakEventManager{T}"/> 的标准实现
