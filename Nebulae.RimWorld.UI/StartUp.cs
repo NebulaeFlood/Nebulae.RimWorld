1b9731d0e114bb2517ba0febc7247e95bfae4af8
NebulaeFlood <2975150170@qq.com>
2025-03-08 21:00:22 +0800

    添加初始化逻辑。

--- Nebulae.RimWorld.UI/StartUp.cs
+++ Nebulae.RimWorld.UI/StartUp.cs
@@ -2,23 +2,52 @@
 using Nebulae.RimWorld.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Verse;
 
 namespace Nebulae.RimWorld.UI
 {
     /// <summary>
-    /// 当资源完全加载时执行的操作的管理器
+    /// 初始化任务管理器
     /// </summary>
     public static class StartUp
     {
+        private static List<Action> _finalQuests = new List<Action>();
         private static List<Action> _startUpQuests = new List<Action>();
 
 
         /// <summary>
-        /// 添加要在资源加载完毕时执行的操作
+        /// 添加要在初始化任务完成后的任务
         /// </summary>
-        /// <param name="action">资源加载完毕时执行的操作</param>
+        /// <param name="action">初始化任务完成后的任务</param>
+        public static void AddFinalQuest(Action action)
+        {
+            if (_finalQuests is null)
+            {
+                return;
+            }
+
+            void WrappedAction()
+            {
+                try
+                {
+                    action.Invoke();
+                }
+                catch (Exception e)
+                {
+                    "NebulaeFlood's Lib".Error(
+                        $"An exception occured when try finish a final quest from {action.Method.DeclaringType}.{action.Method.Name}.\n---> {e}");
+                }
+            }
+
+            _finalQuests.Add(WrappedAction);
+        }
+
+        /// <summary>
+        /// 添加要在资源加载完毕时执行的初始化任务
+        /// </summary>
+        /// <param name="action">资源加载完毕时执行的初始化任务</param>
         public static void AddQuest(Action action)
         {
             if (_startUpQuests is null)
@@ -43,11 +72,18 @@ namespace Nebulae.RimWorld.UI
         }
 
 
-        internal static void FinishQuests()
+
+
+        internal static async void FinishQuestsAsync()
         {
-            _startUpQuests.ForEach(x => Task.Run(x));
+            await Task.WhenAll(_startUpQuests.Select(x => Task.Run(x)).ToArray());
+
             _startUpQuests.Clear();
             _startUpQuests = null;
+
+            _finalQuests.Select(x => Task.Run(x));
+            _finalQuests.Clear();
+            _finalQuests = null;
         }
     }
 }
