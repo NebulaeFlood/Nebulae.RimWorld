af9341aad5547fc902a776d6611b2b1da3852d6e
NebulaeFlood <2975150170@qq.com>
2025-07-10 16:14:16 +0800

    重写部分方法。

--- Nebulae.RimWorld/Utilities/TickUtility.cs
+++ Nebulae.RimWorld/Utilities/TickUtility.cs
@@ -5,38 +5,36 @@ using Verse;
 namespace Nebulae.RimWorld.Utilities
 {
     /// <summary>
-    /// 定义一个可借助 <see cref="TickUtility"/> 每隔 <see cref="TickUtility.IntervalTick"/> 刻执行一次指定方法的对象
+    /// 定义一个可借助 <see cref="TickUtility"/> 以每隔 <see cref="TickUtility.IntervalTick"/> 刻执行一次指定方法的对象
     /// </summary>
-    public interface ITickAvailable
+    public interface IClock
     {
         /// <summary>
         /// 每隔 <see cref="TickUtility.IntervalTick"/> 刻执行一次的方法
         /// </summary>
-        /// <returns>是否将该对象移出 Tick 队列。</returns>
-        bool ProcessTick();
+        /// <param name="interval">每次调用该方法的间隔</param>
+        /// <returns>若为 <see langword="false"/>，则将该对象移出 Tick 队列。</returns>
+        bool Tick(int interval);
 
         /// <summary>
         /// 当该对象被添加到 Tick 队列时执行的方法
         /// </summary>
-        void OnTickStart();
+        void OnStarted();
 
         /// <summary>
         /// 当该对象被移出 Tick 队列时执行的方法
         /// </summary>
-        void OnTickStopped();
+        void OnStopped();
     }
+
 
     /// <summary>
     /// 游戏刻帮助类
     /// </summary>
     public static class TickUtility
     {
-        private static readonly List<ITickAvailable> _objectsNeedToTick = new List<ITickAvailable>();
-
-        private static int _intervalTick = 60;
-
         /// <summary>
-        /// 每次 Tick 的间隔
+        /// 每次执行 <see cref="IClock.Tick"/> 的间隔
         /// </summary>
         public static int IntervalTick
         {
@@ -59,31 +57,33 @@ namespace Nebulae.RimWorld.Utilities
         /// <summary>
         /// 将指定对象添加至 Tick 队列
         /// </summary>
-        /// <param name="obj">要添加到 Tick 队列的对象</param>
-        public static void StartTick(this ITickAvailable obj)
+        /// <param name="clock">要添加到 Tick 队列的对象</param>
+        public static void StartTick(this IClock clock)
         {
-            if (obj is null || _objectsNeedToTick.Contains(obj))
+            if (clock is null || Clocks.Contains(clock))
             {
                 return;
             }
 
-            obj.OnTickStart();
-            _objectsNeedToTick.Add(obj);
+            clock.OnStarted();
+            Clocks.AddLast(clock);
         }
 
         /// <summary>
         /// 将指定对象移出 Tick 队列
         /// </summary>
-        /// <param name="obj">要从 Tick 队列移出的对象</param>
-        public static void StopTick(this ITickAvailable obj)
+        /// <param name="clock">要从 Tick 队列移出的对象</param>
+        public static void StopTick(this IClock clock)
         {
-            if (obj is null)
+            if (clock is null)
             {
                 return;
             }
 
-            _objectsNeedToTick.Remove(obj);
-            obj.OnTickStopped();
+            if (Clocks.Remove(clock))
+            {
+                clock.OnStopped();
+            }
         }
 
 
@@ -99,6 +99,21 @@ namespace Nebulae.RimWorld.Utilities
         }
 
 
+        //------------------------------------------------------
+        //
+        //  Private Static Fields
+        //
+        //------------------------------------------------------
+
+        #region Private Static Fields
+
+        private static readonly LinkedList<IClock> Clocks = new LinkedList<IClock>();
+
+        private static int _intervalTick = 60;
+
+        #endregion
+
+
 
         private sealed class GameComponent_TickHelper : GameComponent
         {
@@ -110,19 +125,24 @@ namespace Nebulae.RimWorld.Utilities
 
             public override void GameComponentTick()
             {
-                int count = _objectsNeedToTick.Count;
-
-                if (count < 1 || ++_tick % _intervalTick > 0)
+                if (Clocks.Count < 1 || ++_tick % _intervalTick > 0)
                 {
                     return;
                 }
 
-                for (int i = count - 1; i >= 0; i--)
+                var node = Clocks.First;
+
+                while (node != null)
                 {
-                    if (_objectsNeedToTick[i].ProcessTick())
+                    if (!node.Value.Tick(_intervalTick))
                     {
-                        _objectsNeedToTick[i].OnTickStopped();
-                        _objectsNeedToTick.RemoveAt(i);
+                        var nextNode = node.Next;
+                        Clocks.Remove(node);
+                        node = nextNode;
+                    }
+                    else
+                    {
+                        node = node.Next;
                     }
                 }
 
@@ -131,7 +151,7 @@ namespace Nebulae.RimWorld.Utilities
 
             public override void StartedNewGame()
             {
-                _objectsNeedToTick.Clear();
+                Clocks.Clear();
             }
         }
     }
