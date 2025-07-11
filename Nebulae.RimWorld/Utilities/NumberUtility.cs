bb758ac4fee4b9cbbac25b542c048843133e7cad
NebulaeFlood <2975150170@qq.com>
2025-07-11 11:23:12 +0800

    移除不必要的方法。

--- Nebulae.RimWorld/Utilities/NumberUtility.cs
+++ Nebulae.RimWorld/Utilities/NumberUtility.cs
@@ -9,31 +9,6 @@ namespace Nebulae.RimWorld.Utilities
     public static class NumberUtility
     {
         /// <summary>
-        /// 对数字向上取整
-        /// </summary>
-        /// <param name="value">要取整的数</param>
-        /// <returns>向上取整后的数字</returns>
-        public static float Ceiling(this float value) => Mathf.Ceil(value);
-
-        /// <summary>
-        /// 限定数字到指定范围
-        /// </summary>
-        /// <param name="value">要限定的数</param>
-        /// <param name="min">数的最小值</param>
-        /// <param name="max">数的最大值</param>
-        /// <returns>限定后的数字</returns>
-        public static float Clamp(this float value, float min, float max) => Mathf.Clamp(value, min, max);
-
-        /// <summary>
-        /// 限定数字到指定范围
-        /// </summary>
-        /// <param name="value">要限定的数</param>
-        /// <param name="min">数的最小值</param>
-        /// <param name="max">数的最大值</param>
-        /// <returns>限定后的数字</returns>
-        public static int Clamp(this int value, int min, int max) => Mathf.Clamp(value, min, max);
-
-        /// <summary>
         /// 限定数字到指定范围
         /// </summary>
         /// <typeparam name="T">数字类型</typeparam>
@@ -53,20 +28,6 @@ namespace Nebulae.RimWorld.Utilities
             }
             return value;
         }
-
-        /// <summary>
-        /// 对数字向下取整
-        /// </summary>
-        /// <param name="value">要取整的数</param>
-        /// <returns>向下取整后的数字</returns>
-        public static float Floor(this float value) => Mathf.Floor(value);
-
-        /// <summary>
-        /// 判断数字是否为无穷
-        /// </summary>
-        /// <param name="value">要判断的数</param>
-        /// <returns>数字是否为无穷</returns>
-        public static bool IsInfinity(this float value) => float.IsInfinity(value);
 
         /// <summary>
         /// 判断数字是否在指定范围内
