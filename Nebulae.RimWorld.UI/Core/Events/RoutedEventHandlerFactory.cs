bc6c3a6a502c1436f2ac20c07f4bf64561be17ed
NebulaeFlood <2975150170@qq.com>
2025-07-13 17:02:07 +0800

    清理代码

--- Nebulae.RimWorld.UI/Core/Events/RoutedEventHandlerFactory.cs
+++ Nebulae.RimWorld.UI/Core/Events/RoutedEventHandlerFactory.cs
@@ -1,7 +1,5 @@
-﻿using RimWorld;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
