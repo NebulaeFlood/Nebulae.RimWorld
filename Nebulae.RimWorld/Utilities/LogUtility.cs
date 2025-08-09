46a0afe761a86ebf4eb6f098951e1923e52e62ca
NebulaeFlood <2975150170@qq.com>
2025-08-09 20:43:03 +0800

    添加将 AsLog 方法，以使 null 值在日志中更加明显

--- Nebulae.RimWorld/Utilities/LogUtility.cs
+++ Nebulae.RimWorld/Utilities/LogUtility.cs
@@ -8,17 +8,37 @@ namespace Nebulae.RimWorld.Utilities
     public static class LogUtility
     {
         /// <summary>
+        /// 将 <paramref name="obj"/> 转化为日志字符
+        /// </summary>
+        /// <param name="obj">要转化为字符的对象</param>
+        /// <returns>由  <paramref name="obj"/> 转化后的字符。</returns>
+        public static string AsLog(this object obj)
+        {
+            if (obj is null)
+            {
+                return "null";
+            }
+
+            if (obj is string str && str.Length < 1)
+            {
+                return $"{typeof(string)}.{nameof(string.Empty)}";
+            }
+
+            return obj.ToString();
+        }
+
+        /// <summary>
         /// 将 <paramref name="obj"/> 提交为日志
         /// </summary>
         /// <param name="obj">要提交为日志的对象</param>
-        public static void Dump(this object obj) => Log.Message(obj);
+        public static void Dump(this object obj) => Log.Message(obj.AsLog());
 
         /// <summary>
         /// 将 <paramref name="obj"/> 提交为日志
         /// </summary>
         /// <param name="obj">要提交为日志的对象</param>
         /// <param name="title">日志的标题</param>
-        public static void Dump(this object obj, string title) => Log.Message($"[{title}] {obj}");
+        public static void Dump(this object obj, string title) => Log.Message($"[{title}] {obj.AsLog()}");
 
         /// <summary>
         /// 以 <paramref name="logLabel"/> 为主语，提交错误
@@ -39,7 +59,7 @@ namespace Nebulae.RimWorld.Utilities
         /// <param name="color"><paramref name="logLabel"/> 要设置的颜色。格式详见 Unity 富文本。</param>
         public static void Message(this string logLabel, object obj, string color = "3F48CCFF")
         {
-            Log.Message($"<color=#{color}>[{logLabel}]</color> {obj}");
+            Log.Message($"<color=#{color}>[{logLabel}]</color> {obj.AsLog()}");
         }
 
         /// <summary>
